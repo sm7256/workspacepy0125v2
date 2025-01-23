be32cb3dc968297d35875a2bbc12ef50d4086e18
gianzk <47899960+gianzk@users.noreply.github.com>
2025-01-23 19:09:17 +0000

    update

---
+++ modulo3/ejercicios/practica/practica.py
@@ -0,0 +1,21 @@
+# Realice un programa que pueda gestionar tickets de buses
+# las clases a usar seran buses  , conductores
+# 1. Un menu iteractivo con las siguiente opciones: agregar bus , agregar ruta a bus 
+# registrar horario a bus, agregar conductor , agregar horario a conductor(*) y asignar bus a conductor(**)
+# * consideremos que el horario de los conductores solo es a nivel de horas mas no dias ni fechas
+# **validar que no haya conductores en ese horario ya asignados
+class Conductor:
+    #horario
+    #
+    pass
+
+class Buses:
+    #conductoresAsignado:Conductor[]
+    #ruta
+    #horarios=[]
+    pass
+
+
+
+class Admin:
+    pass
